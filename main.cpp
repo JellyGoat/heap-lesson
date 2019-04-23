7d8daf44fcac1939bba847fa6f2762aab642b682
JellyGoat <48863570+JellyGoat@users.noreply.github.com>
2019-04-23 17:39:27 +0800

    Update heap code

--- main.cpp
+++ main.cpp
@@ -1,98 +1,78 @@
 #include <iostream>
 #include <cstdio>
 #include <cctype>
+#include <cstring>
 
 using namespace std;
-template <typename T_>
-inline T_ getnum()
-{
-    T_ res = 0;
-    bool flag = false;
-    char ch = getchar();
-    while (!isdigit(ch))
-    {
-        flag = flag ? flag : ch == '-';
-        ch = getchar();
-    }
-    while (isdigit(ch))
-    {
-        res = (res << 3) + (res << 1) + ch - '0';
-        ch = getchar();
-    }
-    return res;
-}
-
-namespace my
-{
-struct heap
-{
-    lli data[100001] = {0};
-    int size_;
-    inline int size() { return this->size_; }
-    //上浮
-    inline void update(int leaf)
-    {
-        //以大根堆为例子
-        while (leaf >= 0)
-        {
-            //若已经满足便退出循环
-            if (data[leaf >> 1] >= data[leaf])
-                break;
-            swap(data[leaf], data[leaf >> 1]);
-            leaf >>= 1;
-        }
-    }
-    //插入
-    inline void insert(lli num)
-    {
-        data[++size_] = num;
-        update(size_);
-    }
-    //最上面的元素必定是最大的
-    inline lli top() { return this->data[1]; }
-    //下沉
-    inline void downdate(int root)
-    {
-        //当root仍然不是叶子结点的时候
-        while ((root << 1) <= size_)
-        {
-            //找一个最大的儿子的下标
-            int y = data[root << 1] >= data[(root << 1) + 1] ? (root << 1) : ((root << 1) + 1);
-            if (data[root] > data[y])
-                break;
-            //比最大的儿子都大，那肯定满足堆序
-            swap(data[y], data[root]);
-            root = y;
-        }
-    }
-    //迎合python习惯，返回堆顶的值
-    inline lli pop()
-    {
-        lli res = data[1];
-        swap(data[1], data[size_]);
-        size_--;
-        downdate(1);
-        return res;
-    }
+typedef long long int lli;
+namespace my{
+inline int lson(int rt){return rt<<1;}
+inline int rson(int rt){return (rt<<1)+1;} 
+inline int rt(int son){return son>>1;}
+struct heap{
+	//数据 
+	lli data[100001];
+	//记录节点个数 
+	int cnt;
+	//初始化 
+	heap()
+	{
+		memset(data,0,sizeof(data));
+		cnt=0;
+	}
+	inline int size(){return this->cnt;}
+	inline void downdate(int p)
+	{
+		while ((p<<1)<=this->cnt)
+		{
+			int son;
+			if (this->data[lson(p)]>=this->data[rson(p)])
+				son=lson(p);
+			else
+				son=rson(p);
+			if (this->data[p]>this->data[son])break;
+			swap(this->data[p],this->data[son]);
+			p=son;
+		}
+	}
+	inline void update(int p)
+	{
+		while (p!=1)
+		{
+			if (this->data[rt(p)]>this->data[p])break;
+			swap(this->data[rt(p)],this->data[p]);
+			p=rt(p);
+		}
+	}
+	inline void push(lli num)
+	{
+		this->data[++this->cnt]=num;
+		this->update(this->cnt);
+	}
+	inline lli top(){return this->data[1];}
+	inline void pop()
+	{
+		this->data[1]=this->data[this->cnt--];
+		this->downdate(1);
+	}
 };
-} // namespace my
+} 
 
 int main()
 {
-#ifdef WIN32
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w+", stdout);
-#endif // WIN32
-    int n;
-    my::heap _;
-    for (register int i = 1; i <= n; i++)
-    {
-        lli num = getnum<lli>();
-        _.insert(num);
-    }
-    for (register int i = 1; i <= n; i++)
-    {
-        cout << _.pop() << ' ';
-    }
-    return 0;
+	int n;
+	my::heap _;
+	cin>>n;
+	for (register int i=1;i<=n;i++)
+	{
+		lli num;
+		cin>>num;
+		_.push(num);
+	}
+	for (register int i=1;i<=n;putchar(' '),i++)
+	{
+		cout<<_.top();
+		_.pop();
+	}
+	return 0;
 }
